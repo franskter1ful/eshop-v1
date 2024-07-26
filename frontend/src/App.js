f85c68ea73e055d5253c9d2b277c77266d4c55e1
franskter1ful <frankster1ful@gmail.com>
2024-07-26 15:08:47 -0700

    test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,13 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div><h1>Welcome to Jhoa Shop</h1></div>
+    <div><h1>Welcome to Jhoa Shop</h1>
+    <ul>
+      <li>item 1</li>
+      <li>item 2</li>
+      <li>item 3</li>
+    </ul>
+    </div>
   )
 }
 
